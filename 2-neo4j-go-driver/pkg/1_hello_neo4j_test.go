e32b4f52f5c9d5fc8f42c409b1e65c765155ded9
Florent Biville <florent.biville@neo4j.com>
2022-05-16 17:34:43 +0200

    Introduce Go driver exercises

---
+++ 2-neo4j-go-driver/pkg/1_hello_neo4j_test.go
@@ -0,0 +1,60 @@
+package workshop_test
+
+import (
+	"context"
+	"fmt"
+	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
+	"github.com/testcontainers/testcontainers-go"
+	workshop "graphconnect/go-driver/pkg"
+	"testing"
+)
+
+const username = "neo4j"
+const password = "s3cr3t"
+
+func TestNeo4jDriverConnectivity(outer *testing.T) {
+
+	ctx := context.Background()
+
+	neo4jContainer, err := workshop.StartNeo4jContainer(ctx, workshop.ContainerConfiguration{
+		Neo4jVersion: "4.4",
+		Username:     username,
+		Password:     password,
+	})
+	if err != nil {
+		outer.Errorf("Could not start container: %v", err)
+	}
+	defer func() {
+		err := neo4jContainer.Terminate(ctx)
+		if err != nil {
+			outer.Fatalf("Could not start container: %v", err)
+		}
+	}()
+
+	outer.Run("creates a Neo4j driver and verify connectivity", func(t *testing.T) {
+		// TODO: fix the createDriver function below
+		driver := createDriver(t, ctx, neo4jContainer)
+		defer func() {
+			if err := driver.Close(); err != nil {
+				t.Fatalf("Could not close driver: %v", err)
+			}
+		}()
+
+		err := driver.VerifyConnectivity()
+
+		if err != nil {
+			t.Fatalf("Expected driver to connect to the container but did not: %v", err)
+		}
+	})
+}
+
+func createDriver(t *testing.T, ctx context.Context, container testcontainers.Container) neo4j.Driver {
+	port, err := container.MappedPort(ctx, "7687")
+	if err != nil {
+		t.Fatalf("Could not get mapped Bolt port: %v", err)
+	}
+	uri := fmt.Sprintf("neo4j://localhost:%d", port.Int())
+	auth := neo4j.BasicAuth(username, password, "")
+	// TODO: create the driver to connect to the running container
+	panic(fmt.Errorf("connect driver to %s with %v", uri, auth))
+}
---
+++ 2-neo4j-go-driver/pkg/container.go
@@ -0,0 +1,45 @@
+package workshop
+
+import (
+	"context"
+	"fmt"
+	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func StartNeo4jContainer(ctx context.Context, config ContainerConfiguration) (testcontainers.Container, error) {
+	version := config.Neo4jVersion
+	request := testcontainers.ContainerRequest{
+		Image:        fmt.Sprintf("neo4j:%s", version),
+		ExposedPorts: []string{"7687/tcp"},
+		Env: map[string]string{
+			"NEO4J_AUTH":                     config.neo4jAuthEnvVar(),
+			"NEO4J_ACCEPT_LICENSE_AGREEMENT": "yes",
+		},
+		WaitingFor: wait.ForLog("Bolt enabled"),
+	}
+	container, err := testcontainers.GenericContainer(ctx,
+		testcontainers.GenericContainerRequest{
+			ContainerRequest: request,
+			Started:          true,
+		})
+	if err != nil {
+		return nil, err
+	}
+	return container, err
+}
+
+type ContainerConfiguration struct {
+	Neo4jVersion string
+	Username     string
+	Password     string
+}
+
+func (config ContainerConfiguration) neo4jAuthEnvVar() string {
+	return fmt.Sprintf("%s/%s", config.Username, config.Password)
+}
+
+func (config ContainerConfiguration) neo4jAuthToken() neo4j.AuthToken {
+	return neo4j.BasicAuth(config.Username, config.Password, "")
+}
